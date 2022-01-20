e4980708d1be9b62486b0622bc0682a67419c1c5
ecaz <me@ecaz.xyz>
2022-01-20 15:41:44 -0700

    Define api log handler where root FastAPI instance exists

--- apps/node/api/main.py
+++ apps/node/api/main.py
@@ -1,12 +1,34 @@
+import logging
+
 from fastapi import FastAPI
 
 from v1 import router as v1_router
 
+from pychain.node.config import settings
+
+
+logging.basicConfig(
+    datefmt="%H:%M:%S",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(settings.log_dir / "api.log"),
+    ],
+)
+log = logging.getLogger(__file__)
+
 
 def create_app():
     api = FastAPI()
-
     api.include_router(v1_router, prefix="/api/v1")
+
+    @api.on_event("startup")
+    async def startup() -> None:
+        log.info("Starting client API")
+
+    @api.on_event("shutdown")
+    async def shutdown() -> None:
+        log.info("Stopping client API")
 
     return api
 
--- apps/node/api/v1/__init__.py
+++ apps/node/api/v1/__init__.py
@@ -1,26 +1,7 @@
-import logging
-
 from fastapi import APIRouter
 
-from pychain.node.config import settings
 
-
-logging.basicConfig(
-    datefmt="%H:%M:%S",
-    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
-    level=logging.INFO,
-    handlers=[
-        logging.FileHandler(settings.log_dir / "api.log"),
-    ],
-)
-
-log = logging.getLogger(__file__)
 router = APIRouter()
-
-
-@router.on_event("startup")
-async def startup() -> None:
-    log.info("Starting client API")
 
 
 from .endpoints import *
